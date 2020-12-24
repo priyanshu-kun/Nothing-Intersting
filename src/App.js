81a5c52eae9826bb84bdefe65fef65e736c1e0af
Kira <priyanshushrama709@gmail.com>
2020-12-24 22:47:47 +0530

    Don't know

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [arr, setArr] = useState(JSON.parse(localStorage.getItem("babe")) || []);
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    setArr([input, ...arr]);
+  }
+
+  const handleChangeInInput = (e) => {
+    setInput(e.target.value)
+  }
+
+  useEffect(() => {
+    setInput("")
+    localStorage.setItem("babe", JSON.stringify(arr))
+  }, [arr, setArr])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <input type="text" placeholder="Add content" value={input} onChange={handleChangeInInput} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <section className="context-box">
+        {
+          !arr.length ? "Your entered data will show here" : arr.map(a => <div className="card">{a}</div>)
+        }
+      </section>
+      <button style={{ marginLeft: "40%", marginTop: "30px", padding: "10px 30px", border: "none", background: "red", color: "#fff", borderRadius: "10px", cursor: "pointer" }} onClick={() => {
+        setArr([]);
+      }}>Delete All</button>
     </div>
   );
 }
